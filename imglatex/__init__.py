61159f4ad9d178462ce55a5e19486651fb8721d5
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2018-02-16 21:55:51 -0500

    Set python support for 3.6 and upwards

--- imglatex/__init__.py
+++ imglatex/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """Top-level package for Image to LaTeX."""
 
 __author__ = """Oscar Arbelaez"""
--- imglatex/cli.py
+++ imglatex/cli.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """Console script for imglatex."""
 
 import click
